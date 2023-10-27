9544e3d87ea72377cc9bf7aeec05320be6000c9a
Yoojun <hereisnotyoojun@outlook.com>
2023-10-27 14:33:07 +0800

    Add ClippingBorder & Fix AcrylicPanel doesnt show

---
+++ source/iNKORE.UI.WPF.Modern/Controls/ClippingBorder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace iNKORE.UI.WPF.Modern.Controls
+{
+    /// <Remarks>
+    ///     As a side effect ClippingBorder will surpress any databinding or animation of 
+    ///         its childs UIElement.Clip property until the child is removed from ClippingBorder
+    /// </Remarks>
+    public class ClippingBorder : Border
+    {
+        static ClippingBorder()
+        {
+            CornerRadiusProperty.OverrideMetadata(typeof(ClippingBorder), new FrameworkPropertyMetadata(new CornerRadius(0), CornerRadiusProperty_ValueChanged));
+        }
+
+        private static void CornerRadiusProperty_ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as ClippingBorder)?.ApplyClip();
+        }
+
+        public ClippingBorder()
+        {
+            this.SizeChanged += ClippingBorder_SizeChanged;
+            ApplyClip();
+        }
+
+        private void ClippingBorder_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ApplyClip();
+        }
+
+        public void ApplyClip()
+        {
+            try
+            {
+                _clipRect = ClipInnerRectConverter.DrawRoundedRectangle(new Rect(this.RenderSize), CornerRadius);
+                if (_clipRect != null)
+                {
+                    this.Clip = _clipRect;
+                }
+            }
+            catch { }
+        }
+
+        private StreamGeometry? _clipRect;
+    }
+}
