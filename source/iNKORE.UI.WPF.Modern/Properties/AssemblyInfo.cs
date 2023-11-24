a33d22363584b8dad23c0e27ea6557247e75858c
NotYoojun <notyoojun@outlook.com>
2023-11-24 18:43:33 +0800

    Sign assemblies with strong-name

--- source/iNKORE.UI.WPF.Modern/Properties/AssemblyInfo.cs
+++ source/iNKORE.UI.WPF.Modern/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Windows.Markup;
 
 [assembly: AssemblyTitle("iNKORE.UI.WPF.Modern")]
 
-[assembly: InternalsVisibleTo("iNKORE.UI.WPF.Modern.Controls")]
+[assembly: InternalsVisibleTo("iNKORE.UI.WPF.Modern.Controls, PublicKey=002400000480000094000000060200000024000052534131000400000100010079425c15485b66fccee121200091aa9712fb3044894e6c09e0278eac59e28d966fda1e6084f86fd80c08e946c5e9da07b7f88e82f936df2eb2a5c7e4ea154243502c17bf805002ebd1997873464ff7c8847b1e2aa02d470864d058ea1383ea3e6f8a0f02c4af08093c36beff569cf04a04a73054c0d3e52f6e2b18e1f98412ce")]
 //[assembly: InternalsVisibleTo("iNKORE.UI.WPF.Modern.MahApps")]
-[assembly: InternalsVisibleTo("MUXControlsTestApp")]
+//[assembly: InternalsVisibleTo("MUXControlsTestApp")]
 
 [assembly: XmlnsPrefix("http://schemas.inkore.net/lib/ui/wpf/modern", "modern")]
 [assembly: XmlnsDefinition("http://schemas.inkore.net/lib/ui/wpf/modern", "iNKORE.UI.WPF.Modern")]
