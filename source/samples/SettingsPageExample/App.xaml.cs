0686fb3af15d45d3691899fccce6e53229893309
NotYoojun <notyoojun@outlook.com>
2024-04-06 23:56:27 +0800

    Changed namespcae

--- source/samples/SettingsPageExample/App.xaml.cs
+++ source/samples/SettingsPageExample/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace CardStyleExample
+namespace SettingsPageExample
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- source/samples/SettingsPageExample/MainWindow.xaml.cs
+++ source/samples/SettingsPageExample/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CardStyleExample
+namespace SettingsPageExample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
