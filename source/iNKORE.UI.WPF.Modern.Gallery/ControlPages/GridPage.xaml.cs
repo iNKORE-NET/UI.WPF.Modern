ce008540e85cd29313e2e2db898109fc75482074
Yoojun Zhou <hereisnotyoojun@outlook.com>
2024-11-03 17:42:57 +0800

    Example code for Grid

--- source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/GridPage.xaml.cs
+++ source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/GridPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Page = iNKORE.UI.WPF.Modern.Controls.Page;
 
 namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
 {
@@ -47,16 +49,65 @@ namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
                 Path = new PropertyPath("Value"),
             });
             Example1.Substitutions = new ObservableCollection<ControlExampleSubstitution> { Substitution1, Substitution2 };
+
+            UpdateExampleCode();
+        }
+
+        private void ColumnSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (this.IsLoaded)
+                UpdateExampleCode();
+        }
+
+        private void RowSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (this.IsLoaded)
+                UpdateExampleCode();
         }
 
         #region Example Code
 
         public void UpdateExampleCode()
         {
-
+            Example1.Xaml = Example1Xaml;
         }
 
-        #endregion
+        public string Example1Xaml => $@"
+<Grid x:Name=""Control1""
+    Width=""240"" Height=""160""
+    Background=""Gray"">
+    <Grid.Resources>
+        <Style TargetType=""Rectangle"">
+            <Setter Property=""Height"" Value=""40"" />
+            <Setter Property=""Width"" Value=""40"" />
+        </Style>
+    </Grid.Resources>
+    <Grid.ColumnDefinitions>
+        <ColumnDefinition Width=""50"" />
+        <ColumnDefinition Width=""Auto"" />
+        <ColumnDefinition />
+    </Grid.ColumnDefinitions>
+    <Grid.RowDefinitions>
+        <RowDefinition Height=""50"" />
+        <RowDefinition Height=""Auto"" />
+        <RowDefinition />
+    </Grid.RowDefinitions>
+    <Rectangle
+        x:Name=""Rectangle1""
+        Grid.Row=""{Grid.GetRow(Rectangle1)}""
+        Grid.Column=""{Grid.GetColumn(Rectangle1)}""
+        Width=""50""
+        Height=""50""
+        Fill=""Red"" />
+    <Rectangle Grid.Row=""1"" Fill=""Blue"" />
+    <Rectangle Grid.Column=""1"" Fill=""Green"" />
+    <Rectangle
+        Grid.Row=""1""
+        Grid.Column=""1""
+        Fill=""Yellow"" />
+</Grid>
+";
 
+        #endregion
     }
 }
