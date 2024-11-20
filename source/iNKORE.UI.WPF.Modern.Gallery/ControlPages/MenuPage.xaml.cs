efc46b59625269b92aa7f82c976c290f29264806
Yoojun Zhou <notyoojun@outlook.com>
2024-11-20 17:03:46 +0800

    Example code for Menu

--- source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/MenuPage.xaml.cs
+++ source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/MenuPage.xaml.cs
@@ -23,16 +23,106 @@ namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
         public MenuPage()
         {
             InitializeComponent();
+            UpdateExampleCode();
         }
 
         #region Example Code
 
         public void UpdateExampleCode()
         {
-
+            Example1.Xaml = Example1Xaml;
+            Example2.Xaml = Example2Xaml;
+            Example3.Xaml = Example3Xaml;
         }
 
         #endregion
 
+        public string Example1Xaml => $@"
+<Menu>
+    <MenuItem Header=""File"">
+        <MenuItem Header=""New"" />
+        <MenuItem Header=""Open..."" />
+        <MenuItem Header=""Save"" />
+        <MenuItem Header=""Exit"" />
+    </MenuItem>
+
+    <MenuItem Header=""Edit"">
+        <MenuItem Header=""Undo"" />
+        <MenuItem Header=""Cut"" />
+        <MenuItem Header=""Copy"" />
+        <MenuItem Header=""Paste"" />
+    </MenuItem>
+
+    <MenuItem Header=""Help"">
+        <MenuItem Header=""About"" />
+    </MenuItem>
+</Menu>
+";
+
+        public string Example2Xaml => $@"
+<Menu>
+    <MenuItem Header=""File"">
+        <MenuItem Header=""New"" InputGestureText=""Ctrl+N"" />
+        <MenuItem Header=""Open..."" InputGestureText=""Ctrl+O"" />
+        <MenuItem Header=""Save"" InputGestureText=""Ctrl+S"" />
+        <MenuItem Header=""Exit"" InputGestureText=""Ctrl+E"" />
+    </MenuItem>
+
+    <MenuItem Header=""Edit"">
+        <MenuItem Header=""Undo"" InputGestureText=""Ctrl+Z"" />
+        <MenuItem Header=""Cut"" InputGestureText=""Ctrl+X"" />
+        <MenuItem Header=""Copy"" InputGestureText=""Ctrl+C"" />
+        <MenuItem Header=""Paste"" InputGestureText=""Ctrl+V"" />
+    </MenuItem>
+
+    <MenuItem Header=""Help"">
+        <MenuItem Header=""About"" InputGestureText=""Ctrl+I"" />
+    </MenuItem>
+</Menu>
+";
+
+        public string Example3Xaml => $@"
+<Menu>
+    <MenuItem Header=""File"">
+        <MenuItem Header=""New"">
+            <MenuItem Header=""Plain Text Document"" />
+            <MenuItem Header=""Rich Text Document"" />
+            <MenuItem Header=""Other Formats..."" />
+        </MenuItem>
+        <MenuItem Header=""Open..."" />
+        <MenuItem Header=""Save"" />
+        <Separator />
+        <MenuItem Header=""Exit"" />
+    </MenuItem>
+
+    <MenuItem Header=""Edit"">
+        <MenuItem Header=""Undo"" />
+        <MenuItem Header=""Cut"" />
+        <MenuItem Header=""Copy"" />
+        <MenuItem Header=""Paste"" />
+    </MenuItem>
+
+    <MenuItem Header=""View"">
+        <MenuItem Header=""Output"" />
+        <Separator />
+        <ui:RadioMenuItem GroupName=""OrientationGroup"" Header=""Landscape"" />
+        <ui:RadioMenuItem
+            GroupName=""OrientationGroup""
+            Header=""Portrait""
+            IsChecked=""True"" />
+        <Separator />
+        <ui:RadioMenuItem GroupName=""SizeGroup"" Header=""Small icons"" />
+        <ui:RadioMenuItem
+            GroupName=""SizeGroup""
+            Header=""Medium icons""
+            IsChecked=""True"" />
+        <ui:RadioMenuItem GroupName=""SizeGroup"" Header=""Large icons"" />
+    </MenuItem>
+
+    <MenuItem Header=""Help"">
+        <MenuItem Header=""About"" />
+    </MenuItem>
+</Menu>
+";
     }
 }
