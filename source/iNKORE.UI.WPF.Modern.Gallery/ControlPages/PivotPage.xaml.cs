b7fa261165e19fb90473e1de29b05c04399cb8bb
Yoojun Zhou <notyoojun@outlook.com>
2024-12-06 20:08:30 +0800

    Example code for Pivot

--- source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/PivotPage.xaml.cs
+++ source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/PivotPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
 {
@@ -9,14 +10,39 @@ namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
             InitializeComponent();
         }
 
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateExampleCode();
+        }
+
+
         #region Example Code
 
         public void UpdateExampleCode()
         {
+            if (!this.IsLoaded) return;
 
+            Example1.Xaml = Example1Xaml;
         }
 
-        #endregion
+        public string Example1Xaml => $@"
+<TabControl ui:PivotHelper.Title=""EMAIL""
+    Style=""{{StaticResource TabControlPivotStyle}}"">
+    <TabItem Header=""All"">
+        <TextBlock Text=""all emails go here."" />
+    </TabItem>
+    <TabItem Header=""Unread"">
+        <TextBlock Text=""unread emails go here."" />
+    </TabItem>
+    <TabItem Header=""Flagged"">
+        <TextBlock Text=""flagged emails go here."" />
+    </TabItem>
+    <TabItem Header=""Urgent"">
+        <TextBlock Text=""urgent emails go here."" />
+    </TabItem>
+</TabControl>
+";
 
+        #endregion
     }
 }
