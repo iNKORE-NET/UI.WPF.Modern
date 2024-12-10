507bd280d927c11736f3674098d74690cd0cb7c2
Yoojun Zhou <notyoojun@outlook.com>
2024-12-10 21:19:26 +0800

    Example code for PopupPlacement

--- source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/PopupPlacementPage.xaml.cs
+++ source/iNKORE.UI.WPF.Modern.Gallery/ControlPages/PopupPlacementPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
 {
@@ -9,14 +10,47 @@ namespace iNKORE.UI.WPF.Modern.Gallery.ControlPages
             InitializeComponent();
         }
 
+        private void RadioButtons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateExampleCode();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateExampleCode();
+        }
+
+
         #region Example Code
 
         public void UpdateExampleCode()
         {
+            if (!this.IsLoaded) return;
 
+            Example1.Xaml = Example1Xaml;
         }
 
-        #endregion
+        public string Example1Xaml => $@"
+<ToggleButton x:Name=""Control1""
+    Content=""Open/close popup"" />
+<Popup x:Name=""Popup1""
+    AllowsTransparency=""True""
+    IsOpen=""{{Binding ElementName=Control1, Path=IsChecked}}""
+    Placement=""{Popup1.Placement}""
+    PlacementTarget=""{{Binding ElementName=Control1}}"">
+    <Border>
+        <ui:ThemeShadowChrome>
+            <Border
+                Width=""100""
+                Height=""100""
+                Background=""{{DynamicResource AcrylicBackgroundFillColorDefaultBrush}}""
+                BorderBrush=""{{DynamicResource SystemControlHighlightAccentBrush}}""
+                BorderThickness=""5"" />
+        </ui:ThemeShadowChrome>
+    </Border>
+</Popup>
+";
 
+        #endregion
     }
 }
