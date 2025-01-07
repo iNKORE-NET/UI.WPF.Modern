9e2a44ac20438253f335649fcfc24964a6d0d20b
Yoojun Zhou <notyoojun@outlook.com>
2025-01-07 17:43:22 +0800

    Example code for TabControl

--- source/iNKORE.UI.WPF.Modern.Gallery/Pages/Controls/Windows/TabViewPage.xaml.cs
+++ source/iNKORE.UI.WPF.Modern.Gallery/Pages/Controls/Windows/TabViewPage.xaml.cs
@@ -22,6 +22,8 @@ namespace iNKORE.UI.WPF.Modern.Gallery.Pages.Controls.Windows
                 tabControl2.Items.Add(CreateNewTab(i));
                 tabControl3.Items.Add(CreateNewTab(i));
             }
+
+            UpdateExampleCode();
         }
 
         private void TabView_Loaded(object sender, RoutedEventArgs e)
@@ -65,14 +67,127 @@ namespace iNKORE.UI.WPF.Modern.Gallery.Pages.Controls.Windows
             return newItem;
         }
 
+        private void ShowHeaderAndFooterCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateExampleCode();
+        }
+
+        private void RadioButtons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateExampleCode();
+        }
+
+
         #region Example Code
 
         public void UpdateExampleCode()
         {
+            if (!this.IsInitialized) return;
 
+            Example1.Xaml = Example1Xaml;
+            Example2.Xaml = Example2Xaml;
+            Example3.Xaml = Example3Xaml;
+            Example4.Xaml = Example4Xaml;
+            Example5.Xaml = Example5Xaml;
         }
 
-        #endregion
+        public string Example1Xaml => $@"
+<TabControl x:Name=""tabControl""
+    TabStripPlacement=""{tabControl.TabStripPlacement}"">
+    <ui:TabControlHelper.TabStripHeader>
+        <Button
+            HorizontalAlignment=""Stretch""
+            VerticalAlignment=""Stretch""
+            ui:FocusVisualHelper.FocusVisualMargin=""0""
+            Content=""Header""
+            Visibility=""{(ShowHeaderAndFooterCheckBox.IsChecked == true ? Visibility.Visible : Visibility.Collapsed)}"" />
+    </ui:TabControlHelper.TabStripHeader>
+    <ui:TabControlHelper.TabStripFooter>
+        <Button
+            HorizontalAlignment=""Stretch""
+            VerticalAlignment=""Stretch""
+            ui:FocusVisualHelper.FocusVisualMargin=""0""
+            Content=""Footer""
+            Visibility=""{(ShowHeaderAndFooterCheckBox.IsChecked == true ? Visibility.Visible : Visibility.Collapsed)}"" />
+    </ui:TabControlHelper.TabStripFooter>
+</TabControl>
+";
 
+        public string Example2Xaml => $@"
+<TabControl x:Name=""TabView4""
+    SelectedIndex=""0"">
+    <TabControl.Items>
+        <TabItem Header=""CMD Prompt"">
+            <ui:TabItemHelper.Icon>
+                <ui:BitmapIcon ShowAsMonochrome=""False"" UriSource=""/Assets/TabViewIcons/cmd.png"" />
+            </ui:TabItemHelper.Icon>
+        </TabItem>
+        <TabItem Header=""Powershell"">
+            <ui:TabItemHelper.Icon>
+                <ui:BitmapIcon ShowAsMonochrome=""False"" UriSource=""/Assets/TabViewIcons/powershell.png"" />
+            </ui:TabItemHelper.Icon>
+        </TabItem>
+        <TabItem Header=""Windows Subsystem for Linux"">
+            <ui:TabItemHelper.Icon>
+                <ui:BitmapIcon ShowAsMonochrome=""False"" UriSource=""/Assets/TabViewIcons/linux.png"" />
+            </ui:TabItemHelper.Icon>
+        </TabItem>
+    </TabControl.Items>
+</TabControl>
+";
+
+        public string Example3Xaml => $@"
+<TabControl SelectedIndex=""0"">
+    <ui:TabControlHelper.TabStripHeader>
+        <TextBlock
+            Margin=""8,6""
+            VerticalAlignment=""Center""
+            Style=""{{DynamicResource BaseTextBlockStyle}}""
+            Text=""TabStripHeader Content"" />
+    </ui:TabControlHelper.TabStripHeader>
+    <ui:TabControlHelper.TabStripFooter>
+        <TextBlock
+            Margin=""6""
+            HorizontalAlignment=""Right""
+            VerticalAlignment=""Center""
+            Style=""{{DynamicResource BaseTextBlockStyle}}""
+            Text=""TabStripFooter Content"" />
+    </ui:TabControlHelper.TabStripFooter>
+</TabControl>
+";
+
+        public string Example4Xaml => $@"
+<TabControl x:Name=""tabControl3"" ui:ThemeManager.HasThemeResources=""True"">
+    <TabControl.Resources>
+        <ui:ResourceDictionaryEx>
+            <ui:ResourceDictionaryEx.ThemeDictionaries>
+                <ResourceDictionary x:Key=""Light"">
+                    <SolidColorBrush x:Key=""TabViewBackground"" Color=""{{DynamicResource SystemAccentColorLight2}}"" />
+                </ResourceDictionary>
+                <ResourceDictionary x:Key=""Dark"">
+                    <SolidColorBrush x:Key=""TabViewBackground"" Color=""{{DynamicResource SystemAccentColorDark2}}"" />
+                </ResourceDictionary>
+            </ui:ResourceDictionaryEx.ThemeDictionaries>
+        </ui:ResourceDictionaryEx>
+    </TabControl.Resources>
+</TabControl>
+";
+
+        public string Example5Xaml => $@"
+<TabControl x:Name=""tabControl2"">
+    <TabControl.Resources>
+        <sys:Double x:Key=""TabViewItemHeaderFontSize"">24</sys:Double>
+        <sys:Double x:Key=""TabViewItemHeaderIconSize"">32</sys:Double>
+    </TabControl.Resources>
+    <TabControl.ItemContainerStyle>
+        <Style BasedOn=""{{StaticResource DefaultTabItemStyle}}"" TargetType=""TabItem"">
+            <Setter Property=""FontFamily"" Value=""Courier New"" />
+        </Style>
+    </TabControl.ItemContainerStyle>
+</TabControl>
+";
+
+
+        #endregion
     }
 }
