c139881d7deccc17209676d462c0459a583049a0
Mohamed Hazem <72768725+mou-haz@users.noreply.github.com>
2025-07-13 01:01:58 +0300

    updated Expander style
    - updated expander style to match winui style
    - common helpers moved to more appropriate paths
    - SettingsExpander Expander animation is now fully in xaml and no longer uses Expander animation helper.

---
+++ source/iNKORE.UI.WPF.Modern/Common/Converters/MultiplyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace iNKORE.UI.WPF.Modern.Common.Converters
+{
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var first = values[0];
+
+            if (first is not double result)
+            {
+                return values;
+            }
+
+            for (int index = 1; index < values.Length; index++)
+            {
+                if (values[index] is double current)
+                {
+                    result *= current;
+                }
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
