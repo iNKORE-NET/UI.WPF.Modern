253983daa1f0851484351a3311524ece8f8c757f
GID <elminuryati12345@gmail.com>
2025-07-20 19:58:20 +0700

    Add TypographyControl.xaml.cs

---
+++ source/iNKORE.UI.WPF.Modern.Gallery/Pages/DesignGuidance/TypographyControl.xaml.cs
@@ -0,0 +1,102 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace iNKORE.UI.WPF.Modern.Gallery.Pages.DesignGuidance
+{
+    public sealed partial class TypographyControl : UserControl
+    {
+        public TypographyControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ExampleProperty = 
+            DependencyProperty.Register(nameof(Example), typeof(string), typeof(TypographyControl), new PropertyMetadata(""));
+
+        public string Example
+        {
+            get => (string)GetValue(ExampleProperty);
+            set => SetValue(ExampleProperty, value);
+        }
+
+        public static readonly DependencyProperty WeightProperty = 
+            DependencyProperty.Register(nameof(Weight), typeof(string), typeof(TypographyControl), new PropertyMetadata(""));
+
+        public string Weight
+        {
+            get => (string)GetValue(WeightProperty);
+            set => SetValue(WeightProperty, value);
+        }
+
+        public static readonly DependencyProperty VariableFontProperty = 
+            DependencyProperty.Register(nameof(VariableFont), typeof(string), typeof(TypographyControl), new PropertyMetadata(""));
+
+        public string VariableFont
+        {
+            get => (string)GetValue(VariableFontProperty);
+            set => SetValue(VariableFontProperty, value);
+        }
+
+        public static readonly DependencyProperty SizeLineHeightProperty = 
+            DependencyProperty.Register(nameof(SizeLineHeight), typeof(string), typeof(TypographyControl), new PropertyMetadata(""));
+
+        public string SizeLineHeight
+        {
+            get => (string)GetValue(SizeLineHeightProperty);
+            set => SetValue(SizeLineHeightProperty, value);
+        }
+
+        public static readonly DependencyProperty ExampleStyleProperty = 
+            DependencyProperty.Register(nameof(ExampleStyle), typeof(Style), typeof(TypographyControl), new PropertyMetadata(null));
+
+        public Style ExampleStyle
+        {
+            get => (Style)GetValue(ExampleStyleProperty);
+            set => SetValue(ExampleStyleProperty, value);
+        }
+
+        public static readonly DependencyProperty ResourceNameProperty = 
+            DependencyProperty.Register(nameof(ResourceName), typeof(string), typeof(TypographyControl), new PropertyMetadata(""));
+
+        public string ResourceName
+        {
+            get => (string)GetValue(ResourceNameProperty);
+            set => SetValue(ResourceNameProperty, value);
+        }
+
+        private void CopyToClipboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(ResourceName))
+                {
+                    Clipboard.SetText(ResourceName);
+                    
+                    // Show confirmation animation
+                    VisualStateManager.GoToState(this, "ConfirmationDialogVisible", true);
+                    
+                    // Hide confirmation after 2 seconds
+                    var timer = new System.Windows.Threading.DispatcherTimer
+                    {
+                        Interval = TimeSpan.FromSeconds(2)
+                    };
+                    timer.Tick += (s, args) =>
+                    {
+                        timer.Stop();
+                        VisualStateManager.GoToState(this, "ConfirmationDialogHidden", true);
+                    };
+                    timer.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle clipboard access errors silently
+                System.Diagnostics.Debug.WriteLine($"Failed to copy to clipboard: {ex.Message}");
+            }
+        }
+    }
+}
