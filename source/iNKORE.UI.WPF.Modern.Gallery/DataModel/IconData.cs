fec3cbb31edb2a7aec4188a4e55a54dd69523046
GID <elminuryati12345@gmail.com>
2025-09-01 11:46:37 +0700

    Refactor IconData class structure and properties

--- source/iNKORE.UI.WPF.Modern.Gallery/DataModel/IconData.cs
+++ source/iNKORE.UI.WPF.Modern.Gallery/DataModel/IconData.cs
@@ -4,37 +4,38 @@
 using System;
 using System.Collections.Generic;
 
-namespace iNKORE.UI.WPF.Modern.Gallery.DataModel;
-
-public class IconData
+namespace iNKORE.UI.WPF.Modern.Gallery.DataModel
 {
-    public string Name { get; set; }
-    public string Code { get; set; }
-    // To check which icon set this icon came from (e.g. "SegoeFluentIcons", "FluentSystemIcons.Regular")
-    public string Set { get; set; }
-    public string[] Tags { get; set; } = Array.Empty<string>();
-    public bool IsSegoeFluentOnly { get; set; }
-
-    public string Character
+    public class IconData
     {
-        get
+        public string Name { get; set; }
+        public string Code { get; set; }
+        // Which icon set this icon came from (e.g. "SegoeFluentIcons", "FluentSystemIcons.Regular")
+        public string Set { get; set; }
+        public string[] Tags { get; set; } = Array.Empty<string>();
+        public bool IsSegoeFluentOnly { get; set; }
+
+        public string Character
         {
-            if (string.IsNullOrWhiteSpace(Code)) return string.Empty;
-            try
+            get
             {
-                int value = Convert.ToInt32(Code, 16);
-                return char.ConvertFromUtf32(value);
-            }
-            catch
-            {
-                return string.Empty;
+                if (string.IsNullOrWhiteSpace(Code)) return string.Empty;
+                try
+                {
+                    int value = Convert.ToInt32(Code, 16);
+                    return char.ConvertFromUtf32(value);
+                }
+                catch
+                {
+                    return string.Empty;
+                }
             }
         }
+
+        public string CodeGlyph => string.IsNullOrWhiteSpace(Code) ? string.Empty : "\\u" + Code;
+        public string TextGlyph => string.IsNullOrWhiteSpace(Code) ? string.Empty : "&#x" + Code + ";";
+
+        // WPF doesn't have Symbol enum like WinUI
+        public string SymbolName => null;
     }
-
-    public string CodeGlyph => string.IsNullOrWhiteSpace(Code) ? string.Empty : "\\u" + Code;
-    public string TextGlyph => string.IsNullOrWhiteSpace(Code) ? string.Empty : "&#x" + Code + ";";
-
-    // WPF doesn't have Symbol enum like WinUI
-    public string SymbolName => null; 
 }
